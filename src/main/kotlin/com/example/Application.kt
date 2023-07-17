352312a589208503e94d1132d22daa625e37f472
Tomasz Białek <bialek.tomasz225@gmail.com>
2023-07-17 15:54:46 +0200

    Session Management, base data models, createRoomRoute API

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,13 +4,35 @@ import io.ktor.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import com.example.routes.createRoomRoute
+import com.example.session.DrawingSession
+import io.ktor.routing.*
+import io.ktor.sessions.*
+import io.ktor.util.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
+val server = DrawingServer()
+
 fun Application.module() {
+    install(Sessions) {
+        cookie<DrawingSession>("SESSION")
+    }
+    intercept(ApplicationCallPipeline.Features) {
+        if (call.sessions.get<DrawingSession>() == null) {
+            val clientId = call.parameters["client_id"] ?: ""
+            call.sessions.set(DrawingSession(clientId, generateNonce()))
+        }
+    }
+
+    install(Routing) {
+        createRoomRoute()
+    }
+
+
     configureSerialization()
     configureSockets()
     configureMonitoring()
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,15 +1,7 @@
 package com.example.plugins
 
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
 
 fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-    }
+
 }
