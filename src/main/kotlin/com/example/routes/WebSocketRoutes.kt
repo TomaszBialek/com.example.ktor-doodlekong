713adedd12dd50f583b6d6d270fb9a63ffa90eaa
Tomasz Białek <bialek.tomasz225@gmail.com>
2023-07-18 19:53:09 +0200

    WebSocket broadcasting DrawData

--- src/main/kotlin/com/example/routes/WebSocketRoutes.kt
+++ src/main/kotlin/com/example/routes/WebSocketRoutes.kt
@@ -1,9 +1,13 @@
 package com.example.routes
 
+import com.example.data.Room
 import com.example.data.models.BaseModel
 import com.example.data.models.ChatMessage
+import com.example.data.models.DrawData
 import com.example.gson
 import com.example.other.Constants.TYPE_CHAT_MESSAGE
+import com.example.other.Constants.TYPE_DRAW_DATA
+import com.example.server
 import com.example.session.DrawingSession
 import com.google.gson.JsonParser
 import io.ktor.http.cio.websocket.*
@@ -11,6 +15,25 @@ import io.ktor.routing.*
 import io.ktor.sessions.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.channels.consumeEach
+
+fun Route.gameWebSocketRoute() {
+    route("/ws/draw") {
+        standardWebSocket { socket, clientId, message, payload ->
+            when(payload) {
+                is DrawData -> {
+                    val room = server.rooms[payload.roomName] ?: return@standardWebSocket
+                    if (room.phase == Room.Phase.GAME_RUNNING) {
+                        room.broadcastToAllExcept(message, clientId)
+
+                    }
+                }
+                is ChatMessage -> {
+
+                }
+            }
+        }
+    }
+}
 
 fun Route.standardWebSocket(
     handleFrame: suspend (
@@ -34,6 +57,7 @@ fun Route.standardWebSocket(
                     val jsonObject = JsonParser.parseString(message).asJsonObject
                     val type = when(jsonObject.get("type").asString) {
                         TYPE_CHAT_MESSAGE -> ChatMessage::class.java
+                        TYPE_DRAW_DATA -> DrawData::class.java
                         else -> BaseModel::class.java
                     }
 
