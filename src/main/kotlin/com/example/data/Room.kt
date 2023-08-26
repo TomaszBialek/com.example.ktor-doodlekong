a7972edc9ec9379ecb5a29bce29fefa80600c0af
Tomasz Białek <bialek.tomasz225@gmail.com>
2023-08-26 19:49:01 +0200

    Word guess, drawing player icon bug

--- src/main/kotlin/com/example/data/Room.kt
+++ src/main/kotlin/com/example/data/Room.kt
@@ -256,6 +256,7 @@ class Room(
     private fun gameRunning() {
         winningPlayers = listOf()
         val wordToSend = word ?: currentWords?.random() ?: words.random()
+        word = wordToSend
         val wordWithUnderscores = wordToSend.transformToUnderscores()
         val drawingUsername = (drawingPlayer ?: players.random()).username
         val gameStateForDrawingPlayer = GameState(
@@ -393,6 +394,8 @@ class Room(
 
         if (drawingPlayerIndex < players.size - 1) drawingPlayerIndex ++
         else drawingPlayerIndex = 0
+
+        drawingPlayer?.isDrawing = true
     }
 
     private fun kill() {
